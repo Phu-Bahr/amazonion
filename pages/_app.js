95730c800950de3f133f87b4b06e83a76fc0f224
Tony Phu <tphu118@gmail.com>
2021-01-06 12:34:18 -0500

    initial starting template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import '../styles/globals.scss';
 
+//global.scss can only be imported here and only 1 global scss file can exist, the rest needs to be moddules
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
