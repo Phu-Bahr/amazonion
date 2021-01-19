a12b4c9da9bd61fd5cab6127cfebfae3c6b8a79c
Tony Phu <tphu118@gmail.com>
2021-01-19 13:51:16 -0500

    removed flex and installed grid layout instead

--- components/Charts/MonthlyChart.js
+++ components/Charts/MonthlyChart.js
@@ -31,7 +31,7 @@ export const MonthlyChart = ({ data, year }) => {
   const yearData = [
     {
       id: 'Month Total',
-      color: 'hsl(336, 70%, 50%)',
+
       data: filteredYear(coordinates, year),
     },
   ];
@@ -61,7 +61,7 @@ export const MonthlyChart = ({ data, year }) => {
     <section className='chart'>
       <ResponsiveLine
         data={yearData}
-        margin={{ top: 50, right: 110, bottom: 50, left: 70 }}
+        margin={{ top: 50, right: 50, bottom: 50, left: 80 }}
         xScale={{ type: 'point' }}
         yScale={{
           type: 'linear',
@@ -93,10 +93,12 @@ export const MonthlyChart = ({ data, year }) => {
           legendPosition: 'middle',
           format: (v) => `$${v}`,
         }}
+        lineWidth={4}
         enableSlices='x'
         motionConfig={'wobbly'}
         enablePointLabel={true}
         enableArea={true}
+        areaOpacity={0.7}
         enableGridX={false}
         pointSize={10}
         pointColor={{ theme: 'background' }}
@@ -104,32 +106,6 @@ export const MonthlyChart = ({ data, year }) => {
         pointBorderColor={{ from: 'serieColor' }}
         pointLabelYOffset={-12}
         useMesh={true}
-        legends={[
-          {
-            anchor: 'bottom-right',
-            direction: 'column',
-            justify: false,
-            translateX: 100,
-            translateY: 0,
-            itemsSpacing: 0,
-            itemDirection: 'left-to-right',
-            itemWidth: 80,
-            itemHeight: 20,
-            itemOpacity: 0.75,
-            symbolSize: 12,
-            symbolShape: 'circle',
-            symbolBorderColor: 'rgba(0, 0, 0, .5)',
-            effects: [
-              {
-                on: 'hover',
-                style: {
-                  itemBackground: 'rgba(0, 0, 0, .03)',
-                  itemOpacity: 1,
-                },
-              },
-            ],
-          },
-        ]}
       />
     </section>
   );
