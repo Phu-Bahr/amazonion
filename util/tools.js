86d1979dc71c8c1d4b668b2f5b20b921dbb43f0d
Tony Phu <tphu118@gmail.com>
2021-01-20 16:45:43 -0500

    started initial summary data

--- util/tools.js
+++ util/tools.js
@@ -35,3 +35,43 @@ export const convertData = (array) => {
 export const getYearList = (array) => {
   return [...new Set(array.map((element) => element['Order Year']))];
 };
+
+//filter data by year
+export const filteredYear = (array, chosenYear) => {
+  let pickedYear =
+    array && array.filter((yr) => yr['Order Year'] === chosenYear);
+  return pickedYear;
+};
+
+export const CommaFormatted = (amount) => {
+  var delimiter = ','; // replace comma if desired
+  var a = amount.split('.', 2);
+  var d = a[1];
+  var i = parseInt(a[0]);
+  if (isNaN(i)) {
+    return '';
+  }
+  var minus = '';
+  if (i < 0) {
+    minus = '-';
+  }
+  i = Math.abs(i);
+  var n = new String(i);
+  var a = [];
+  while (n.length > 3) {
+    var nn = n.substr(n.length - 3);
+    a.unshift(nn);
+    n = n.substr(0, n.length - 3);
+  }
+  if (n.length > 0) {
+    a.unshift(n);
+  }
+  n = a.join(delimiter);
+  if (d.length < 1) {
+    amount = n;
+  } else {
+    amount = n + '.' + d;
+  }
+  amount = minus + amount;
+  return amount;
+};
