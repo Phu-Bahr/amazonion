3e002d794fee0aa3ba2b2c4d5cb915285dd47997
Tony Phu <tphu118@gmail.com>
2021-01-22 22:27:30 -0500

    adding ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
-import '../styles/globals.scss';
+// //global.scss can only be imported here and only 1 global scss file can exist, the rest needs to be moddules
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
 
-//global.scss can only be imported here and only 1 global scss file can exist, the rest needs to be moddules
-function MyApp({ Component, pageProps }) {
+// export default MyApp;
+
+import '../styles/globals.scss';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
+
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
